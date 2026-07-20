33eb0579f922b99cbb39d342c0a49d1a4a1da94c
azizbrghol10-sketch <azizbrghol10@gmail.com>
2026-07-20 22:45:55 +0300

    Create manifest.json, sw.js

---
+++ manifest.json, sw.js
@@ -0,0 +1,13 @@
+{
+  "name": "هاند - لعبة ورق جماعية",
+  "short_name": "هاند",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#33401c",
+  "theme_color": "#4a5a2a",
+  "orientation": "portrait",
+  "icons": [
+    { "src": "icon-192.png", "sizes": "192x192", "type": "image/png" },
+    { "src": "icon-512.png", "sizes": "512x512", "type": "image/png" }
+  ]
+}
